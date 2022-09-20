0a7d6337f1d11f08fff004a65415a4ff622d6ac9
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2022-09-20 22:32:10 +0100

    Added paging to npm run

--- npm-run.js
+++ npm-run.js
@@ -8,6 +8,7 @@ readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
 const packageJsonPath = "./package.json";
+const pageSize = 5;
 
 if (!fs.existsSync(packageJsonPath)) {
     console.log("No package.json file found. Exiting.");
@@ -53,13 +54,41 @@ function logScripts(scripts) {
 logScripts(packageScripts);
 
 process.stdin.on("keypress", (keyString, keyObj) => {
-    if (keyObj.name === "down" && currentIndex < packageScripts.length - 1) {
+    if (keyObj.name === "down") {
+        if (!(currentIndex < packageScripts.length - 1)) {
+            return;
+        }
+
         currentIndex++;
         logScripts(packageScripts);
-    } else if (keyObj.name === "up" && currentIndex > 0) {
+        return;
+    }
+
+    if (keyObj.name === "up") {
+        if (currentIndex <= 0) {
+            return;
+        }
+
         currentIndex--;
         logScripts(packageScripts);
-    } else if (keyObj.name === "return") {
+        return;
+    }
+
+    if (keyObj.name === "pagedown") {
+        currentIndex += pageSize;
+        currentIndex = Math.min(currentIndex, packageScripts.length - 1);
+        logScripts(packageScripts);
+        return;
+    }
+
+    if (keyObj.name === "pageup") {
+        currentIndex -= pageSize;
+        currentIndex = Math.max(currentIndex, 0);
+        logScripts(packageScripts);
+        return;
+    }
+
+    if (keyObj.name === "return") {
         console.clear();
 
         // a random comment
@@ -71,7 +100,11 @@ process.stdin.on("keypress", (keyString, keyObj) => {
 
             process.exit(0);
         });
-    } else if (keyObj.name === "c" && keyObj.ctrl) {
+
+        return;
+    }
+
+    if ((keyObj.name === "c" && keyObj.ctrl) || keyObj.name === "escape") {
         process.exit(0);
     }
 });
